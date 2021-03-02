6bd6a695ad1d7e6e13fa7406ec37c9a879aeac66
thillaig <thillaig@teezle.com>
2021-03-02 15:55:00 +0000

    initial od myapp

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var app = require('../index.js');
 describe('GET /', function() {
     it('Respond with hello world', function(done) {
         //navigate to root and check the the response is "hello world"
-        request(app).get('/kd').expect({
+        request(app).get('/').expect({
             status : true,
-            message : 'Hello World'
+            message : 'HI-1'
         }, done);
     });
 });
